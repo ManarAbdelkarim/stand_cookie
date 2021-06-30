d33279ad32d31e52ef6078c4ba2f7cdc68c93b33
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-06-30 23:34:05 +0300

    second lab

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from '../components/Head'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+import { useState } from 'react'
+
+
+export default function Home() {
+
+  const [title,setTitle] = useState('Cookie Stand Admin');
+  const [branches,setBranches] = useState('0')
+  const [path,setPath] = useState("/overview")
+  const [page,setPage] = useState("overview")
+
+  return (
+    <div className="bg-green-100">
+      <Head title={title}/>
+      <Header header={title} path={path} page={page}/>
+      <Main title={title} setBranches={setBranches}/>
+      <Footer branches={branches}/>
+    </div>
+  )
+}
+
+
+
+  
