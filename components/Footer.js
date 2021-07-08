759421ea4d8c71628a08596601dbe896f6fef32d
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-07-08 21:48:12 +0300

    counter added

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,7 @@
-export default function Footer(props) {
+export default function Footer({cookieStands}) {
     return (
-        <footer className="className=p-5 text-center bg-green-500  h-15">
-            <p className="p-3">
-           &#169;2021
-            </p>
+        <footer className="">
+            <p className=' ml-4 p-3' >  Locations World Wide {cookieStands} here </p>
         </footer>
     )
 }
